a18a61672d7dad7389d18039ffeb11633979950f
Janne <bhi256@myy.haaga-helia.fi>
2024-10-15 17:23:38 +0300

    Tests added

--- src/TodoTable.jsx
+++ src/TodoTable.jsx
@@ -2,14 +2,14 @@ export default function TodoTable(props) {
   return (
     <div>
       <table id="todotable"><tbody>
-      {
-        props.todos.map((todo, index) => 
-          <tr key={index}>
-            <td>{todo.date}</td>
-            <td>{todo.desc}</td>
-          </tr>)
+        {
+          props.todos.map((todo, index) =>
+            <tr key={index}>
+              <td>{todo.date}</td>
+              <td>{todo.desc}</td>
+            </tr>)
         }
-        </tbody></table>    
+      </tbody></table>
     </div>
   )
 }
